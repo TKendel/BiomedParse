7836ec254794cad18959e59fd1415013c46a9d3f
TKendel <kendeltoni@gmail.com>
2025-03-14 09:51:22 +0100

    feat: added 3D CT and MRI + volumentric dsc, iou, hd

--- inference_utils/output_processing.py
+++ inference_utils/output_processing.py
@@ -1,8 +1,15 @@
-import json
-from scipy import stats
-import numpy as np
-
 import huggingface_hub
+import json
+import numpy as np
+import torch
+
+from scipy import stats
+from skimage.metrics import hausdorff_distance
+from reprlib import recursive_repr
+from functools import partial
+from typing import Callable, Iterable, List, Set, Tuple, TypeVar, cast
+from torch import Tensor, einsum
+from scipy.spatial import distance, cKDTree
 
 
 def check_mask_stats(img, mask, modality_type, target):
@@ -88,7 +95,99 @@ def combine_masks(predicts):
     
     return masks
 
+
+
 def get_target_dist():
     huggingface_hub.hf_hub_download('microsoft/BiomedParse', filename='target_dist.json', local_dir='./inference_utils')
     huggingface_hub.hf_hub_download('microsoft/BiomedParse', filename="config.yaml", local_dir="./configs")
     return json.load(open("inference_utils/target_dist.json"))
+
+
+
+def one_hot(t: Tensor, axis=0) -> bool:
+    return sset(t, [0,1])
+    # return simplex(t, axis) and sset(t, [0, 1])
+
+
+
+# Check if there are multiple classes per pixel, axis indicating the number of classes( our case there is only one)
+def simplex(t: Tensor, axis=0) -> bool:
+    _sum = cast(Tensor, t.sum(axis).type(torch.float32))
+    _ones = torch.ones_like(_sum, dtype=torch.float32)
+    return torch.allclose(_sum, _ones)
+
+
+
+# Assert utils
+def uniq(a: Tensor) -> Set:
+    return set(torch.unique(a.cpu()).numpy())
+
+
+
+def sset(a: Tensor, sub: Iterable) -> bool:
+    return uniq(a).issubset(sub)
+
+
+
+def intersection(a: Tensor, b: Tensor) -> Tensor:
+    return a * b
+
+
+
+# Metrics
+def meta_dice(sum_str: str, label: Tensor, pred: Tensor, smooth: float = 1e-8) -> Tensor:
+    assert label.shape == pred.shape
+    assert one_hot(label) #Check if the tensor consists of only 0-1 and that only one class is present per pixel
+    assert one_hot(pred)
+            
+    inter_size: Tensor = einsum(sum_str, [intersection(label, pred)]).type(torch.float32)
+    sum_sizes: Tensor = (einsum(sum_str, [label]) + einsum(sum_str, [pred])).type(torch.float32)
+
+    dices: Tensor = (2 * inter_size + smooth) / (sum_sizes + smooth)
+
+    return dices
+
+
+
+# With this einsum subscript we take the axis ij and all 
+# the rest and sum over all of them while keeping j off them 
+dice_volume = partial(meta_dice, "swh->")
+
+def meta_iou(sum_str: str, label: Tensor, pred: Tensor, smooth: float = 1e-8) -> Tensor:
+    assert label.shape == pred.shape
+    assert one_hot(label)
+    assert one_hot(pred)
+
+    inter_size: Tensor = einsum(sum_str, [intersection(label, pred)]).type(torch.float32) # Get only the intersection between the two image tensors
+    union_size: Tensor = (einsum(sum_str, [label]) + einsum(sum_str, [pred]) - inter_size).type(torch.float32) # Get the sum of the whole volume for pred and label but make sure to level with inner untion
+
+    ious: Tensor = (inter_size + smooth) / (union_size + smooth)
+
+    return ious
+
+iou_volume = partial(meta_iou, "swh->")
+
+
+
+def max_min_distance_chunked(source, target, chunk_size):
+    max_min_dist = -np.inf
+    tree = cKDTree(target)  # Build KDTree for fast nearest neighbor search
+    for i in range(0, len(source), chunk_size):
+        chunk = source[i:i + chunk_size]
+        dists, _ = tree.query(chunk)  # Find nearest distances
+        max_min_dist = max(max_min_dist, np.max(dists))
+    return max_min_dist
+
+def hausdorff_distance_volume(label, pred, chunk_size=1000):
+    # Get coordinates of non 0 values
+    p1 = np.array(np.where(label)).T
+    p2 = np.array(np.where(pred)).T
+
+    if len(p1) == 0 or len(p2) == 0:
+        return np.inf
+
+    # Compute Hausdorff distance
+    hd1 = max_min_distance_chunked(p1, p2, chunk_size) 
+    hd2 = max_min_distance_chunked(p2, p1, chunk_size)
+    
+    return max(hd1, hd2)
