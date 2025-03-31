fa2618e095f7b8a39b7e575f08831613aad4b21e
TKendel <kendeltoni@gmail.com>
2025-03-31 10:34:10 +0200

    feat: removed buffer since its adding unnecassery overhead

--- inference_utils/processing_utils.py
+++ inference_utils/processing_utils.py
@@ -207,12 +207,11 @@ def get_instances(mask):
 
 
 def volume_trimmer(GT):
-    buffer = int(GT.shape[2] * 0.05)
+    # buffer = int(GT.shape[2] * 0.05)
     non_zero = np.unique(np.where(GT>0)[2])
 
     if non_zero.size == 0:
         return None
     else:
-        first, last = non_zero[0], non_zero[-1]
+        return non_zero[0], non_zero[-1]
 
-        return GT[:, :, first-buffer : last+buffer]
