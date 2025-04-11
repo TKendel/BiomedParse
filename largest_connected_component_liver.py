4e27004e5194f96dd316e0bd9fc88a03cba25c8c
TKendel <kendeltoni@gmail.com>
2025-04-11 11:19:36 +0200

    feat: scripts for extracting kidneys and liver

--- largest_connected_component_liver.py
+++ largest_connected_component_liver.py
@@ -1,22 +1,24 @@
 import glob
-import numpy as np
-import os
-import matplotlib.pyplot as plt
-from scipy import ndimage
 import nibabel as nib
+import numpy as np
+
+from scipy.ndimage import label, binary_erosion
+
 from inference_utils.processing_utils import read_nifti_only, volume_trimmer
-# import matplotlib.image as Image
+
 
 PDAC_patients = [100002, 100005, 100011, 100028, 100030, 100033, 100043, 100050, 100060, 100074, 100082, 100091, 100096, 100101, 100102, 100107, 100124, 100127, 100134, 100138]
 for p in PDAC_patients:
     img_paths = f'results_pub\\CT_patient_liver_{p}.nii.gz'
     for img_path in glob.iglob(img_paths):
-        pred, nii = read_nifti_only(img_path)
+        pred, nii_ = read_nifti_only(img_path)
         image, nii = read_nifti_only(f'data\\CT\\img\\{p}_00001_0000.nii.gz')
 
         print(pred.shape)
 
-        labels, features = ndimage.label(pred)
+        eroded_pred = binary_erosion(pred, iterations=1).astype(pred.dtype)
+
+        labels, features = label(eroded_pred)
         segmentation_sizes = np.bincount(labels.ravel())
         segmentation_sizes[0] = 0
 
@@ -27,12 +29,13 @@ for p in PDAC_patients:
         first, last = volume_trimmer(largest_segmentation)
 
         buffer = int(image.shape[2] * 0.1)
-        image = image[:,:, first+buffer:last+buffer]
 
-        imgplot = plt.imshow(image[:,:,0])
-        plt.show()
-        imgplot = plt.imshow(image[:,:,-1])
-        plt.show()
-        exit()
-        final_img = nib.Nifti1Image(largest_segmentation, nii.affine)
-        nib.save(final_img, f'results_pub\\CT_patient_liver_only_{p}.nii.gz')
+        if image[:,:, first].sum() > image[:,:, last].sum():
+
+            image = image[:,:, first+buffer:last+buffer]
+            final_img = nib.Nifti1Image(largest_segmentation, nii.affine)
+            nib.save(final_img, f'results_pub\\CT_patient_liver_only_{p}.nii.gz')
+        else:
+            image = image[:,:, first-buffer:last-buffer]
+            final_img = nib.Nifti1Image(largest_segmentation, nii.affine)
+            nib.save(final_img, f'results_pub\\CT_patient_liver_only_{p}.nii.gz')
